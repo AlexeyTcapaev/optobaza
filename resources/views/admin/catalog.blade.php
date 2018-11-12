8cbf1267f119fd806255f2b61fa435491f128b1b
Цапаев Алексей <gonzolexx@gmail.com>
2018-11-13 01:02:28 +0300

    some for category

--- resources/views/admin/catalog.blade.php
+++ resources/views/admin/catalog.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.admin')
 @section('content')
-
+<ul class="collection">
+    <li class="collection-item admin-catalog">Alvina<div class="actions"><a class="btn">Удалить</a><a class="btn">Редактировать</div></a></li>
+    <li class="collection-item">Alvin</li>
+    <li class="collection-item">Alvin</li>
+    <li class="collection-item">Alvin</li>
+</ul>
+<div class="input-field col s12">
+    <input  id="first_name" type="text" class="validate">
+    <label for="first_name">Добавить категорию</label>
+</div>
 @endsection
